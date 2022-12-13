22300e61faebe82ebf1b42afdbee9e83f13d180d
Fedor <fedor.kochkin48@gmail.com>
2022-12-13 07:00:22 +0300

    решение задачи 23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int b = 0;
+Console.WriteLine("введите a");
+int a = int.Parse(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+{
+    b = i * i * i;
+Console.WriteLine(b); 
+}
