9e4e4656bb6f061e28489a8551b3d792f3d4c8fa
Fedor <fedor.kochkin48@gmail.com>
2022-12-13 06:52:12 +0300

    решение задачи 19

---
+++ zadacha19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int pal = 0;
+int b = 0;
+Console.WriteLine("введите a");
+int a = int.Parse(Console.ReadLine());
+b = a;
+while (a > 0)
+{
+pal = (pal * 10) + (a % 10);
+a = a / 10;
+}
+if ( b == pal)
+ Console.WriteLine("палиндром"); 
+ else 
+  Console.WriteLine("не палиндром"); 
